e982edefdad75e6a4f2961f7c78f7a1d6401e5ee
Amitesh <amitesh.sharma.2001@gmail.com>
2020-06-29 13:37:55 -0700

    added a login page and css file, two npm installs

--- social-media/src/App.js
+++ social-media/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import './App.css';
+import Login from './Pages/Login';
+
 
 function App() {
   return (
-    <div className="App">
-        Social media App!
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route to="/" component={Login} />
+        </Switch>  
+      </div>
+    </Router>
   );
 }
 
---
+++ social-media/src/Pages/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Css/Login.css';
+
+class Login extends React.Component {
+  render() {
+    return (
+        <h1>hi</h1>  
+      );
+  }
+}
+
+export default Login;
