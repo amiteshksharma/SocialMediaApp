dc0476b7931301dbb1de022a90391725cae73a73
Amitesh <amitesh.sharma.2001@gmail.com>
2020-06-30 02:58:05 -0700

    login page sketch is completed

--- social-media/src/App.js
+++ social-media/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import Login from './Pages/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
--- social-media/src/Pages/Login.js
+++ social-media/src/Pages/Login.js
@@ -1,10 +1,49 @@
 import React from 'react';
 import '../Css/Login.css';
+import Form from 'react-bootstrap/Form';
+import { Container, Row, Col, Button } from 'react-bootstrap'; 
 
 class Login extends React.Component {
   render() {
     return (
-        <h1>hi</h1>  
+        <div className="Login">
+          <div className="login-form">
+            <Container>
+              <Row>
+                <Col md={{ span: 6, offset: 3}}>
+                  <h2>LOGIN</h2>
+                </Col>
+              </Row>
+
+              <Row>
+                <Col md={{ span: 6, offset: 3 }}>
+                  <Form>
+                    <Form.Group controlId="formGroupEmail">
+                      <div><Form.Label>Email address</Form.Label></div>
+                      <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+                    <Form.Group controlId="formGroupPassword">
+                      <div><Form.Label>Password</Form.Label></div>
+                      <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                  </Form>
+                </Col>
+              </Row>
+
+              <Row>
+                <Col md={{ span: 6, offset: 3}}>
+                  <Button as="input" type="button" value="Login" className="w-100 mt-3"/>{' '}
+                </Col>
+              </Row>
+
+              <Row>
+                <Col md={{ span: 6, offset: 5}}>
+                  <footer className="forgot-password">Forgot Password?</footer>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        </div>
       );
   }
 }
