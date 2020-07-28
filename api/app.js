530cf87be0a45d7f27614d45526aa181a762c255
Amitesh Sharma <amitesh.sharma.2001@gmail.com>
2020-07-28 00:20:19 -0700

    changes www file... run build works on my computer

--- api/app.js
+++ api/app.js
@@ -41,9 +41,9 @@ app.use("/backend/create", backendRouter);
 app.use("/settings", profileRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
 app.use(function(err, req, res, next) {
